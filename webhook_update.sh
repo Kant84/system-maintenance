a3d54ee9a1a2c30c458548ff7ad2acb0fd3cf77f
ANDREI <157600260+Kant84@users.noreply.github.com>
2025-11-26 16:14:55 +0300

    Update webhook_update.sh

--- webhook_update.sh
+++ webhook_update.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+RAW="https://raw.githubusercontent.com/Kant84/system-maintenance/main/system-maintenance"
+
+curl -o /usr/local/bin/system_maintenance.sh $RAW/system_maintenance.sh
+chmod +x /usr/local/bin/system_maintenance.sh
+
+systemctl daemon-reload
+
+echo "Обновление выполнено."
 
