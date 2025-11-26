8faa58c9a4cf1a823b5bf9aeabe83c0f90aaee6e
ANDREI <157600260+Kant84@users.noreply.github.com>
2025-11-26 17:07:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ RepoName="system-maintenance"
 Branch="main"
 RAW="https://raw.githubusercontent.com/$GitHubUser/$RepoName/$Branch/system-maintenance"
 
-# Скачиваем основной скрипт
 curl -o /usr/local/bin/system_maintenance.sh $RAW/system_maintenance.sh
 chmod +x /usr/local/bin/system_maintenance.sh
 
-# Скачиваем systemd юниты
 curl -o /etc/systemd/system/system-maintenance.service $RAW/system-maintenance.service
 curl -o /etc/systemd/system/system-maintenance.timer $RAW/system-maintenance.timer
 
@@ -17,4 +15,3 @@ systemctl daemon-reload
 systemctl enable --now system-maintenance.timer
 
 echo "Установка завершена."
-
